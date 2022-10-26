3792acc55498f8b3fc8aab37bf1ffdafcb5d060f
muxa1508 <muxa1508@inbox.ru>
2022-10-26 13:16:00 +0300

    Release

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class Main {
+
+    public static final String REMOTE_SERVICE_URL = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static void main(String[] args) {
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("My service test")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();) {
+            HttpGet request = new HttpGet(REMOTE_SERVICE_URL);
+            CloseableHttpResponse response = httpClient.execute(request);
+
+//            Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+//
+//            String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+//            System.out.println(body);
+
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            List<Cats> cats = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<Cats>>() {});
+            for (int i = 0; i < cats.size(); i++) {
+                if (cats.get(i).getUpvotes() > 0) {
+                    System.out.println(cats.get(i).toString());
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
